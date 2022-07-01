fd5876f471f3d9669f5d9155159d8e57e9df5dc7
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-07-01 13:43:07 +0430

    feat(): calculate total price and weight

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,19 @@
 import styles from "./header.module.css";
-import img from "../../assets/logo.svg";
 import { Avatar, Badge } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import { NavLink } from "react-router-dom";
+import { useStore } from "../../store/store";
+import DropdownMenu from "../Dropdown/Dropdown";
 const Header = () => {
+  const products = useStore((state) => state.productsBasket);
+
+  const totalPrice = products.reduce((total, currentValue) => {
+    return total + currentValue.price;
+  }, 0);
+
+  const totalWeight = products.reduce((total, currentValue) => {
+    return total + currentValue.weight;
+  }, 0);
   return (
     <header className={styles.container}>
       <section className={styles.left}>
@@ -51,9 +61,16 @@ const Header = () => {
           className={styles.badge}
           showZero
           color="green"
-          count={30}
+          count={products.length}
           size="default"
         >
+          {products.length === 0 ? null : (
+            <DropdownMenu
+              items={products.length}
+              totalPrice={totalPrice}
+              totalWeight={totalWeight}
+            />
+          )}
           <ShoppingCartOutlined style={{ fontSize: "2rem" }} size={30} />
         </Badge>
       </section>
