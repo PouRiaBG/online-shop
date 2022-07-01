11e3c15f491e522a7262a233b801c09aa691621c
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-07-01 13:39:45 +0430

    feat(): add sortMenu to shop page

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,9 +1,11 @@
 import { ProductList } from "../../components/ProductList/ProductList";
 import styles from "./shop.module.css";
+import { SortMenu } from "../../components/SortMenu/SortMenu";
 
 const Shop = () => {
   return (
     <div className={styles.container}>
+      <SortMenu />
       <ProductList />;
     </div>
   );
